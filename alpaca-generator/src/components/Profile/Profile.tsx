194f39aad6229a7ed0f47233d58243a6ddaf7418
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2024-02-24 10:31:21 +0530

    Add profile skeleton

--- alpaca-generator/src/components/Profile/Profile.tsx
+++ alpaca-generator/src/components/Profile/Profile.tsx
@@ -1,8 +1,8 @@
 import styles from './Profile.module.css';
-import { useAlpacaContext } from '@contexts/AlpacaContextProvider';
+import { useAlpacaContext } from '@/contexts/AlpacaContextProvider';
 
 function Profile() {
-  const {background} = useAlpacaContext();
+  const {background,ears,neck,leg,nose,hair,mouth,eyes,accessories} = useAlpacaContext();
 
   return (
     <div className={styles.profile}>
@@ -10,20 +10,36 @@ function Profile() {
         <img src={background} alt="background" />
       </div>
       
-      <div className="ears"></div>
-      <div className="neck"></div>
-      <div className="leg"></div>
+      <div className="ears">
+        <img src={ears} alt="ears" />
+      </div>
+      <div className="neck">
+        <img src={neck} alt="neck" />
+      </div>
+      <div className="leg">
+      <img src={leg} alt="leg" />
+      </div>
       
-      <div className="nose"></div>
+      <div className="nose">
+        <img src={nose} alt="nose" />
+      </div>
 
-      <div className="hair"></div>
+      <div className="hair">
+      <img src={hair} alt="hair" />
+      </div>
 
-      <div className="mouth"></div>
-      <div className="eyes"></div>
+      <div className="mouth">
+        <img src={mouth} alt="mouth" />
+      </div>
+      <div className="eyes">
+        <img src={eyes} alt="eyes" />
+      </div>
 
-      <div className="accessories"></div>
+      <div className="accessories">
+        {accessories && <img src={accessories} alt="accessories" />}
+      </div>
     </div>
   )
 }
 
-export default Profile
+export default Profile;
--- alpaca-generator/src/contexts/AlpacaContextProvider.tsx
+++ alpaca-generator/src/contexts/AlpacaContextProvider.tsx
@@ -2,19 +2,62 @@ import { ReactNode, createContext, useContext, useState } from "react";
 
 interface AlpacaProps{
     background?: string;
+    ears?: string;
+    neck?: string;
+    leg?: string;
+    nose?: string;
+    hair?: string;
+    mouth?: string;
+    eyes?: string;
+    accessories?: string;
     setBackground?: React.Dispatch<React.SetStateAction<string>>;
+    setEars?: React.Dispatch<React.SetStateAction<string>>;
+    setNeck?: React.Dispatch<React.SetStateAction<string>>;
+    setLeg?: React.Dispatch<React.SetStateAction<string>>;
+    setNose?: React.Dispatch<React.SetStateAction<string>>;
+    setHair?: React.Dispatch<React.SetStateAction<string>>;
+    setMouth?: React.Dispatch<React.SetStateAction<string>>;
+    setEyes?: React.Dispatch<React.SetStateAction<string>>;
+    setAccessories?: React.Dispatch<React.SetStateAction<string>>;
 }
-const temp:AlpacaProps = {};
-export const AlpacaContext = createContext(temp);
+const defaultValue:AlpacaProps = {};
+export const AlpacaContext = createContext(defaultValue);
 
 interface AlpacaProviderProps{
     children?: ReactNode;
 }
 
 export const AlpacaProvider = ({children}: AlpacaProviderProps) => {
-    const [background, setBackground] = useState<string>('@alpaca-assets/backgrounds/darkblue70');
+    const [background, setBackground] = useState<string>("src/assets/alpaca/backgrounds/darkblue70.png");
+    const [ears, setEars] = useState<string>("src/assets/alpaca/ears/default.png");
+    const [neck, setNeck] = useState<string>("src/assets/alpaca/neck/default.png");
+    const [leg, setLeg] = useState<string>("src/assets/alpaca/leg/default.png");
+    const [nose, setNose] = useState<string>("src/assets/alpaca/nose.png");
+    const [hair, setHair] = useState<string>("src/assets/alpaca/hair/default.png");
+    const [mouth, setMouth] = useState<string>("src/assets/alpaca/mouth/default.png");
+    const [eyes, setEyes] = useState<string>("src/assets/alpaca/eyes/default.png");
+    const [accessories, setAccessories] = useState<string>("");
 
-    const values = { background, setBackground};
+    const values = { 
+        background,
+        ears,
+        neck,
+        leg,
+        nose,
+        hair,
+        mouth,
+        eyes,
+        accessories,
+        setBackground,
+        setEars,
+        setNeck,
+        setLeg,
+        setNose,
+        setHair,
+        setMouth,
+        setEyes,
+        setAccessories
+    };
 
     return (
         <AlpacaContext.Provider value={values}>
@@ -27,7 +70,7 @@ export const useAlpacaContext = () => {
     const context = useContext(AlpacaContext);
 
     if(!context){
-        throw new Error("useAlpacaContext must be used inside the AlpacaProvider");
+        console.error("useAlpacaContext must be used inside the AlpacaProvider");
     }
 
     return context;
